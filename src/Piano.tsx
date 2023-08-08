c4372a9ba7069f8708afa217c20e87a21260f0e3
Ben Hollis <ben@benhollis.net>
2023-08-08 14:46:08 -0700

    Keys

--- src/Piano.tsx
+++ src/Piano.tsx
@@ -1,3 +1,45 @@
+import './Piano.css';
+
+const notes = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#'];
+interface PianoKeyData {
+  baseNote: 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G';
+  note: string;
+  sharp: boolean;
+  octave: number;
+}
+
+function makeKeys() {
+  const keys: PianoKeyData[] = [];
+
+  // Assumes the keyboard starts with A0
+  for (let i = 0; i < 88; i++) {
+    const note = notes[i % notes.length];
+    const sharp = note.endsWith('#');
+    const baseNote = note[0] as PianoKeyData['baseNote'];
+    const octave = Math.floor(i / 12);
+    keys.push({
+      note: `${note}${octave}`,
+      baseNote,
+      sharp,
+      octave: Math.floor(i / 12),
+    });
+  }
+
+  return keys;
+}
+
+const keys = makeKeys();
+
 export default function Piano() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      {keys.map(({ note, sharp }) => (
+        <Key key={note} black={sharp} />
+      ))}
+    </>
+  );
+}
+
+function Key({ black }: { black: boolean }) {
+  return <div className={`key ${black ? 'black' : 'white'}`}>{black ? 'Black' : 'White'}</div>;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import register from 'preact-custom-element';
 import Piano from './Piano';
 
-register(Piano, 'flash-piano', ['name'], { shadow: false });
+register(Piano, 'flash-piano', [], { shadow: false });
